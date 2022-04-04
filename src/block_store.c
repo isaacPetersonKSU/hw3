4d3f14be43bac64f8762f504a55ef91918f3e6ca
isaacPetersonKSU <ijp@ksu.edu>
2022-04-04 13:00:16 -0500

    we now pass all tests on implemented methods, but storage IO has not been implemented yet.

--- src/block_store.c
+++ src/block_store.c
@@ -12,16 +12,23 @@ block_store_t *block_store_create()
 {
     // calloc memory for the block store
     block_store_t *block_store = (block_store_t *) calloc(1, sizeof(block_store_t));
+    printf("\tblock store allocated\n");
+    
     // create and store the bitmap in block 127
     block_store->bitmap = bitmap_overlay(BITMAP_SIZE_BITS, &block_store->blocks[BITMAP_START_BLOCK]);
+    printf("\tbitmap %08x created at block block %d\n", *bitmap_export(block_store->bitmap), BITMAP_START_BLOCK);
 
+    /*
     // confirm that the block store was created successfully
     for (int i = BITMAP_START_BLOCK; i <= BITMAP_START_BLOCK + BLOCK_STORE_NUM_BLOCKS; i++) {
+        printf("\tChecking block_store[%d]", i);
         if (block_store_request(block_store, i) == false) {
-            printf("Error with requested block at location: %d", i);
+            printf("\t\tError with requested block at location: %d\n", i);
             break;
         }
+        else printf("\t\tSuccess!\n");
     }
+    */
     return block_store;
 }
 
@@ -29,7 +36,7 @@ void block_store_destroy(block_store_t *const bs)
 {
     // error check
     if (bs == NULL) {
-        printf("Error with destroying block store: block store is empty");
+        printf("Error with destroying block store: block store is empty\n");
         return;
     }
 
@@ -41,48 +48,42 @@ size_t block_store_allocate(block_store_t *const bs)
 {
     // error checking (null, make sure after ffz that the block is valid block within (BLOCK_STORE_NUM_BLOCKS))
     if (bs == NULL) {
-        printf("Error with allocating block store: block store is empty");
-        return 0;
+        printf("ERROR: Unable to allocate blocks in NULL Block Store\n");
+        return SIZE_MAX;
     }
 
     size_t empty = bitmap_ffz(bs->bitmap);
-
-    // error check
-    if (empty > BLOCK_STORE_NUM_BLOCKS) {
-        printf("Error with allocating block store: bit is outside of range");
-        return 0;
+    if(empty < BLOCK_STORE_AVAIL_BLOCKS)
+    {
+        bitmap_set(bs->bitmap, empty);
+        return empty;
     }
-
-    bitmap_set(bs->bitmap, empty);
-    return empty;
+    return SIZE_MAX;
 }
 
 bool block_store_request(block_store_t *const bs, const size_t block_id)
 {
     // error checking with bitmap_test, valid params, within range,
-    if (bs == NULL || block_id == 0) {
-        printf("Error with requesting block store: invalid parameters");
+    if (bs == NULL || block_id >= BLOCK_STORE_AVAIL_BLOCKS) {
+        printf("Error with requesting block store: invalid parameters\n");
         return false;
     }
 
     if (bitmap_test(bs->bitmap, block_id)) {
-        printf("Error with requesting block store: block is taken");
+        printf("Error with requesting block store: block is taken\n");
         return false;
     }
 
     bitmap_set(bs->bitmap, block_id);
 
-    if (bitmap_test(bs->bitmap, block_id)) return true;
-
-    printf("Error with requesting block store: end of method");
-    return false;
+    return bitmap_test(bs->bitmap, block_id);
 }
 
 void block_store_release(block_store_t *const bs, const size_t block_id)
 {
     // error checking
-    if (bs == NULL || block_id == 0) {
-        printf("Error with releasing block store: invalid parameters");
+    if (bs == NULL || block_id >= BLOCK_STORE_AVAIL_BLOCKS) {
+        printf("Error with releasing block store: invalid parameters\n");
         return;
     }
 
@@ -91,19 +92,19 @@ void block_store_release(block_store_t *const bs, const size_t block_id)
 
 size_t block_store_get_used_blocks(const block_store_t *const bs)
 {
-    UNUSED(bs);
-    return 0;
+    if(bs == NULL) return SIZE_MAX;
+    return bitmap_total_set(bs->bitmap);
 }
 
 size_t block_store_get_free_blocks(const block_store_t *const bs)
 {
-    UNUSED(bs);
-    return 0;
+    if(bs == NULL) return SIZE_MAX;
+    return BLOCK_STORE_AVAIL_BLOCKS - bitmap_total_set(bs->bitmap);
 }
 
 size_t block_store_get_total_blocks()
 {
-    return 0;
+    return BLOCK_STORE_AVAIL_BLOCKS;
 }
 
 size_t block_store_read(const block_store_t *const bs, const size_t block_id, void *buffer)
